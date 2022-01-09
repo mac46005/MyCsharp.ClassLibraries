00cfff9c3cbeab436fe21fd6fc9b06023579593c
Marco Preciado <mac46005@gmail.com>
2022-01-09 10:19:05 -0600

    Generic DataService Interface

--- DataManipulation.ClassLib/Interfaces/IDataService.cs
+++ DataManipulation.ClassLib/Interfaces/IDataService.cs
@@ -1,6 +1,14 @@
-namespace DataManipulation.ClassLib.Interfaces{
-    
-    public interface IDataService{
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
+namespace DataManipulation.ClassLib.Interfaces{
+
+    public interface IDataService<T>
+    {
+        Task<T> Get(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Create(T entity);
+        Task<T> Update(int id,T Entity);
+        Task<bool> Delete(int id);
     }
 }
